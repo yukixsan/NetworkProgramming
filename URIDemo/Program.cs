55e2b8d477ee98d8381a4bd5340f958c1bda494f
Rizky <rizky@pens.ac.id>
2022-09-13 00:01:11 +0700

    Add project files.

---
+++ URIDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace URIDemo
+{
+    class Program
+    {
+        public static Uri GetSimpleUri()
+        {
+            var builder = new UriBuilder();
+            builder.Scheme = "http";
+            builder.Host = "google.com";
+            return builder.Uri;
+        }
+
+        static void Main(string[] args)
+        {
+            var simpleUri = GetSimpleUri();
+            Console.WriteLine(simpleUri.ToString());
+
+            Thread.Sleep(10000);
+        }
+    }
+}
---
+++ UriBuilderDemo/DNSProgramDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace UriBuilderDemo
+{
+    class DNSProgramDemo
+    {
+        static void Main(string[] args)
+        {            
+            
+            var domainEntry = Dns.GetHostEntry("kompas.com");
+  
+            Console.WriteLine(domainEntry.HostName);
+            foreach (var ip in domainEntry.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+
+            Console.WriteLine(Dns.GetHostName());
+            IPAddress ipadd = IPAddress.Parse("142.251.10.95");
+            var ipEntry = Dns.GetHostEntry(ipadd);
+            foreach (var ip in ipEntry.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+
+            Thread.Sleep(10000);
+
+        }
+    }
+}
