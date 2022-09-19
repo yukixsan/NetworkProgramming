8c165f133289f855b16ca2434fd49e3ed81d0b50
Rizky <rizky@pens.ac.id>
2022-09-19 23:02:03 +0700

    Moving Object

---
+++ MovingObject/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingObject
+{
+    public partial class Form1 : Form
+    {
+
+        Pen red = new Pen(Color.Red);
+        Rectangle rect = new Rectangle(20, 20, 30, 30);
+        SolidBrush fillBlue = new SolidBrush(Color.Blue);
+        int slide = 10; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            back();
+
+            rect.X += slide;
+            Invalidate();
+        }
+
+        private void back()
+        {
+            if (rect.X >= this.Width - rect.Width * 2)
+                slide = -10;
+            else
+            if (rect.X <= rect.Width / 2)
+                slide = 10;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.DrawRectangle(red, rect);
+            g.FillRectangle(fillBlue, rect);
+        }
+    }
+}
